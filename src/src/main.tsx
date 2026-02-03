951900768ce78bda45ef48f796dc6037940cc850
jsanchezarias <jsanchezarias@gmail.com>
2026-02-03 08:25:18 -0500

    fix: resolver React hydration error y aplicar fondo negro con CSS inline

--- src/src/main.tsx
+++ src/src/main.tsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import '../styles/globals.css'
 
-// Asegurar que el body tenga el fondo oscuro
-document.documentElement.classList.add('dark');
-document.body.style.backgroundColor = '#0d0d0d';
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
